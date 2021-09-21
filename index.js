59f20ec68a9a6101df619da77c30d5b5166cf95e
Vadim Cvirko <promiti90@gmail.com>
2021-09-21 11:09:44 +0300

    настройка HBS

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+const expHbs = require('express-handlebars')
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+const hbs = expHbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
 
 app.listen(port, () => {
   console.log(`server running ${port}`);
